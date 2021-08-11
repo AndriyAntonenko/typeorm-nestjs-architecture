f4e781bf4e08ec1beebe697b61153ee3432a88c7
AndriyAntonenko <andriyantonenko3.16@gmail.com>
2021-08-11 10:11:25 +0300

    Add configuration files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -8,17 +9,23 @@ import { UserOrmEntity } from './repository/user/user.orm-entity';
 
 @Module({
   imports: [
-    ControllerModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'qwerty',
-      database: 'postgres',
-      entities: [UserOrmEntity],
-      synchronize: true,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST'),
+        port: +configService.get<number>('POSTGRES_PORT'),
+        username: configService.get('POSTGRES_USER'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        database: configService.get('POSTGRES_DB'),
+        entities: [UserOrmEntity],
+        synchronize: true,
+      }),
     }),
+
+    ControllerModule,
   ],
   controllers: [AppController],
   providers: [AppService],
